6bd07bb08a0c925df9ae5eaa72464a1bea70a334
Vangala Aruna <arunajava567@gmail.com>
2025-07-22 10:59:46 +0530

    Add files via upload

---
+++ day2/AccountDemo.java
@@ -0,0 +1,30 @@
+package com.verizon.training.day2;
+
+public class AccountDemo {
+
+	public static void main(String[] args) {
+		
+		Account account=new Account();  //instatiation
+		Account account1=new Account(7898,"kiran",25000.00); 
+		System.out.println(Account.bankName);
+		System.out.println(account.bankName);
+		System.out.println(account1.bankName);
+		
+		account.deposit(5000);
+		//System.out.println(name);
+		System.out.println(account.name);
+		account.getBalance();
+		account1.deposit(5000);
+		account1.getBalance();
+	//	System.out.println(account.acNumber);
+		
+		//System.out.println(account1.acNumber+"  "+account1.name);
+		//Account account2=new Account(7348,"rajkiran"); 
+		
+		//System.out.println(account2.name+"  "+account2.acNumber);
+		//System.out.println(account1);
+		//account=null;
+		//System.gc();
+	}
+
+}
