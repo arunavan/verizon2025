88a762976498368964f8851b2ca5118cb462bd3f
Vangala Aruna <arunajava567@gmail.com>
2025-07-23 12:28:44 +0530

    Add files via upload

--- day3/exceptions/Ex5Demo.java
+++ day3/exceptions/Ex5Demo.java
@@ -1,9 +1,19 @@
 package day3.exceptions;
 
-public class Ex5Demo {
+import java.io.IOException;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Ex5Demo {
+	
+	void disp() throws InterruptedException,IOException{
+		Ex5.show();
+	}
+
+	public static void main(String[] args) throws InterruptedException,IOException{
+		
+		//Ex5 e1=new Ex5();
+		
+		Ex5.show();
+		
 
 	}
 
