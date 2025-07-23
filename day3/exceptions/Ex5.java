8bb57393a43617f2f08ff73f49ecd1b9db7804f5
Vangala Aruna <arunajava567@gmail.com>
2025-07-23 12:28:08 +0530

    Add files via upload

--- day3/exceptions/Ex5.java
+++ day3/exceptions/Ex5.java
@@ -1,10 +1,22 @@
 package day3.exceptions;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Ex5 {
+	static void show() throws InterruptedException,IOException
+	{
+		Thread.sleep(5000);
+		
+		FileReader f=new FileReader("C:\\Javatraining\\product.txt");
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	}
 
+	public static void main(String[] args) throws InterruptedException,IOException
+	{
+		
+	   Ex5.show();
+	
 	}
 
 }
