fd6833bee7ae088b08b77059674207df84c3ed07
Vangala Aruna <arunajava567@gmail.com>
2026-05-06 22:13:19 +0530

    Add files via upload

---
+++ oops/abstraction/EdulationLoan.java
@@ -0,0 +1,10 @@
+package oops.abstraction;
+
+import oops.main.Loan;
+public class EdulationLoan  extends Loan{
+	public int getEmi(int amount) {
+		System.out.println(emi);
+		return amount*1000/5;
+		show();
+	}
+}
